6601a0932391bfb0c81eb0e237224c1e6a169eb2
GCGsauce <gattigcg1@gmail.com>
2020-09-23 07:53:13 -0400

    playing sounds works

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +22,9 @@ class Xylophone extends StatefulWidget {
 }
 
 class _XylophoneState extends State<Xylophone> {
+
+  final audioCache = AudioCache(prefix: 'assets/');
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -28,43 +32,57 @@ class _XylophoneState extends State<Xylophone> {
         children: [
           Expanded(
             child: FlatButton(
-              onPressed: (){},
+              onPressed: (){
+                audioCache.play('note1.wav');
+              },
               color: Colors.red,
             )
           ),
           Expanded(
             child: FlatButton(
-              onPressed: (){},
+              onPressed: (){
+                audioCache.play('note2.wav');
+              },
               color: Colors.orange,
             )
           ),
           Expanded(
               child: FlatButton(
-                onPressed: (){},
+                onPressed: (){
+                  audioCache.play('note3.wav');
+                },
                 color: Colors.yellow,
               )
           ),
           Expanded(
               child: FlatButton(
-                onPressed: (){},
+                onPressed: (){
+                  audioCache.play('note4.wav');
+                },
                 color: Colors.green,
               )
           ),
           Expanded(
               child: FlatButton(
-                onPressed: (){},
+                onPressed: (){
+                  audioCache.play('note5.wav');
+                },
                 color: Colors.green[800],
               )
           ),
           Expanded(
               child: FlatButton(
-                onPressed: (){ },
+                onPressed: (){
+                  audioCache.play('note6.wav');
+                },
                 color: Colors.blue,
               )
           ),
           Expanded(
               child: FlatButton(
-                onPressed: (){},
+                onPressed: (){
+                  audioCache.play('note7.wav');
+                },
                 color: Colors.deepPurple,
               )
           ),
